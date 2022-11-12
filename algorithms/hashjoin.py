21e87df1d43de132b6a21f979b4bbce0dce3467f
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2022-11-12 14:58:55 +0800

    Revert "index nest loop join"
    This reverts commit d3af555726b11a07b8b47647b306903312e715f2.

--- algorithms/hashjoin.py
+++
@@ -1,5 +0,0 @@
-
-def hashjoin(hashcost, mergecost, nestloopcost):
-    print("This join is implemented using hash join operator as merge join and NL joins \
-        increase the estimated cost by at least " + int(mergecost/hashcost) + " times and " \
-            + int(nestloopcost/hashcost) + " times, respectively")
