c5f3219869a7f15931a79af5d751c4a8c7411095
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2022-11-12 15:09:37 +0800

    update some annotations

--- algorithms/indexNLjoin.py
+++ algorithms/indexNLjoin.py
@@ -1,6 +1,6 @@
 
 def indexnestloopjoin(hashcost, mergecost, nestloopcost):
-    annotation = "This join is implemented using NL joins operator as hash join and merge join \
+    annotation = "This join is implemented using index NL joins operator as hash join and merge join \
         increase the estimated cost by at least " + int(hashcost/nestloopcost) + " times and " \
             + int(mergecost/nestloopcost) + " times, respectively"
 
