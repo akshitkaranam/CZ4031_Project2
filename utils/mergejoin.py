fd90f9f6f08a844d4bf783752d540eed3e5e04ce
ruoqing11 <ruoqing11@users.noreply.github.com>
2022-11-12 14:04:50 +0800

    natural lang annotations

--- utils/mergejoin.py
+++
@@ -1,5 +0,0 @@
-
-def mergejoin(hashcost, mergecost, nestloopcost):
-    print("This join is implemented using merge join operator as hash join and NL joins \
-        increase the estimated cost by at least " + int(hashcost/mergecost) + " times and " \
-            + int(nestloopcost/mergecost) + " times, respectively")
