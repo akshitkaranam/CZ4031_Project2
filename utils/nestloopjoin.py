f11fd2a0b3fa8a08012a46f7c5e2d72a92b29f30
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2022-11-12 15:07:31 +0800

    remove annotation from utils

--- utils/nestloopjoin.py
+++
@@ -1,5 +0,0 @@
-
-def nestloopjoin(hashcost, mergecost, nestloopcost):
-    print("This join is implemented using NL joins operator as hash join and merge join \
-        increase the estimated cost by at least " + int(hashcost/nestloopcost) + " times and " \
-            + int(mergecost/nestloopcost) + " times, respectively")
