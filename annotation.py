9d3c5d7a81c66b6657d622b8397951d67abdc6ec
akshitkaranam <78993046+akshitkaranam@users.noreply.github.com>
2022-11-13 02:13:32 +0800

    some bug fixes

--- annotation.py
+++ annotation.py
@@ -15,7 +15,7 @@ PARAMS = {
 }
 
 
-def get_all_plans(query_number, disable_methods=()):
+def get_all_plans(query_number):
     query = queries.getQuery(query_number)
     statements = sqlparse.split(query)
     formatted_query = sqlparse.format(statements[0], reindent=True, keyword_case='upper')
@@ -66,9 +66,11 @@ def get_all_plans(query_number, disable_methods=()):
             total_cost = 0
             for node in nodes:
                 total_cost += node.cost
+            optimal_dict.update({index: [operation, total_cost]})
             print(index, operation, total_cost)
     print()
     print()
+    hashjoin_dict = {}
     if hashjoin:
         for line_index in hashjoin:
             index = line_index["index"]
@@ -77,9 +79,37 @@ def get_all_plans(query_number, disable_methods=()):
             total_cost = 0
             for node in nodes:
                 total_cost += node.cost
-            print(index,operation,total_cost)
+            hashjoin_dict.update({index: [operation, total_cost]})
+            print(index, operation, total_cost)
     print()
     print()
+    nestedloop_dict = {}
+    if nestedloop:
+        for line_index in hashjoin:
+            index = line_index["index"]
+            operation = line_index["operation"]
+            nodes = line_index["nodes"]
+            total_cost = 0
+            for node in nodes:
+                total_cost += node.cost
+            nestedloop_dict.update({index: [operation, total_cost]})
+            print(index, operation, total_cost)
+    print()
+    print()
+    indexjoin_dict = {}
+    if nestedloop:
+        for line_index in hashjoin:
+            index = line_index["index"]
+            operation = line_index["operation"]
+            nodes = line_index["nodes"]
+            total_cost = 0
+            for node in nodes:
+                total_cost += node.cost
+            indexjoin_dict.update({index: [operation, total_cost]})
+            print(index, operation, total_cost)
+    print()
+    print()
+    mergejoin_dict = {}
     if mergejoin:
         for line_index in mergejoin:
             index = line_index["index"]
@@ -88,9 +118,68 @@ def get_all_plans(query_number, disable_methods=()):
             total_cost = 0
             for node in nodes:
                 total_cost += node.cost
-            print(index,operation,total_cost)
+            mergejoin_dict.update({index: [operation, total_cost]})
+            print(index, operation, total_cost)
     print()
     print()
+    seqscan_dict = {}
+    if seqscan:
+        for line_index in mergejoin:
+            index = line_index["index"]
+            operation = line_index["operation"]
+            nodes = line_index["nodes"]
+            total_cost = 0
+            for node in nodes:
+                total_cost += node.cost
+            seqscan_dict.update({index: [operation, total_cost]})
+            print(index, operation, total_cost)
+    print()
+    print()
+    indexscan_dict ={}
+    if indexscan:
+        for line_index in mergejoin:
+            index = line_index["index"]
+            operation = line_index["operation"]
+            nodes = line_index["nodes"]
+            total_cost = 0
+            for node in nodes:
+                total_cost += node.cost
+            indexscan_dict.update({index: [operation, total_cost]})
+            print(index, operation, total_cost)
+    print()
+    print()
+    bitmapscan_dict = {}
+    if bitmapscan:
+        for line_index in mergejoin:
+            index = line_index["index"]
+            operation = line_index["operation"]
+            nodes = line_index["nodes"]
+            total_cost = 0
+            for node in nodes:
+                total_cost += node.cost
+            bitmapscan_dict.update({index: [operation, total_cost]})
+            print(index, operation, total_cost)
+    print()
+    print()
+
+
+    for index in optimal_dict.values():
+        optimal_operation = optimal_dict[index][0]
+        nestedloop_operation = nestedloop_dict[index][0]
+        indexjoin_operation = indexjoin_dict[index][0]
+        mergejoin_operation = mergejoin_dict[index][0]
+        hashjoin_operation = hashjoin_dict[index][0]
+        seqscan_operation = seqscan_dict[index][0]
+        bitmapscan_operation = bitmapscan_dict[index][0]
+        indexscan_operation = indexscan_dict[index][0]
+
+        if(nestedloop_operation != optimal_operation):
+            optimal_dict[index].append()
+
+
+
+
+
 
 if __name__ == '__main__':
     mapping = get_all_plans(5)
